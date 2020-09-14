f2221e06759d2cf4be9bb12996de8949685c9e61
gpandey24in <g_pandey24in@yahoo.com>
2020-09-14 13:13:09 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Azure.EventGrid;
+using Microsoft.Azure.EventGrid.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EventPublisher
+{
+    class Program
+    {
+        private const string topicEndPoint = "https://hrtopicgopal.eastus-1.eventgrid.azure.net/api/events";
+        private const string topicKey = "E6KKSQzxkV68QycFCkwYqIoJuHEJjSqOxQ5V9QMlHdI=";
+        public static async Task Main(string[] args)
+        {
+            TopicCredentials credentials = new TopicCredentials(topicKey);
+
+            EventGridClient client = new EventGridClient(credentials);
+
+            List<EventGridEvent> events = new List<EventGridEvent>();
+
+            var firstperson = new
+            {
+                FullName = "Amit Sharma",
+                Address = "123 Pine Acen, San Francisco, CA,USA"
+            };
+
+            EventGridEvent firstEvent = new EventGridEvent
+            {
+                Id = Guid.NewGuid().ToString(),
+                EventType = "Employees.Registration.New",
+                EventTime = DateTime.Now,
+                Subject = $"New Employee: {firstperson.FullName}",
+                Data = firstperson.ToString(),
+                DataVersion = "1.0.0"
+            };
+            events.Add(firstEvent);
+
+            var secondPerson = new
+            {
+                FullName = "Alex Devix",
+                Address = "95 Cottege Lane, lbvd, CA 95107"
+            };
+
+            EventGridEvent secondEvent = new EventGridEvent
+            {
+                Id = Guid.NewGuid().ToString(),
+                EventType = "Employees.Registration.New",
+                EventTime = DateTime.Now,
+                Subject = $"New Employee: {secondPerson.FullName}",
+                Data = secondPerson.ToString(),
+                DataVersion = "1.0.0"
+            };
+            events.Add(secondEvent);
+
+            string topicHostName = new Uri(topicEndPoint).Host;
+
+            await client.PublishEventsAsync(topicHostName, events);
+
+            Console.WriteLine("Events published to web  ");
+
+        }
+
+
+    }
+}
